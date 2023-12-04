b1e5ebc6df1366b3506d9d4876dd8af0a891fab3
Numan Al Rashid <85104468+mnalrashid@users.noreply.github.com>
2023-12-04 19:15:19 +0000

    Update run.py
    Stock Menu Updated

--- run.py
+++ run.py
@@ -31,12 +31,21 @@ def wrong_selection():
     print()
     return True
 
+def not_enough_stock():
+    print()
+    print('Not enough stock, please RESTOCK')
+    print()
+
 def check_stock():
     stock_worksheet = SHEET.worksheet('stock')
     stocks = stock_worksheet.get_all_values()
-    last_stock = stocks[-1]    
-    last_stock = [int(x) for x in last_stock]
-    return last_stock
+    if len(stocks) >=2:
+        last_stock = stocks[-1]  
+        last_stock = [int(x) for x in last_stock]
+        return last_stock
+    else:
+        not_enough_stock()
+        restock()
 
 def update_sales(drinks, amount):
     sales = [drinks[0],drinks[1],drinks[2],amount]
@@ -58,38 +67,38 @@ def update_stock(stock):
 def item_added(value, amount):
     if value == '1':
         drinks[0] = drinks[0] + 1
-        amount = amount + 3.20
+        amount = round(amount + 3.20,2)
         stock[0] = stock[0] + 40
         stock[2] = stock[2] + 10
         sales_menu(drinks, amount)
     elif value == '2':
         drinks[1] = drinks[1] + 1
-        amount = amount + 4.50
+        amount = round(amount + 4.50,2)
         stock[0] = stock[0] + 30
         stock[1] = stock[1] + 20
         stock[2] = stock[2] + 20
         sales_menu(drinks, amount)
     elif value == '3':
         drinks[2] = drinks[2] + 1
-        amount = amount + 5.00
+        amount = round(amount + 5.00,2)
         stock[0] = stock[0] + 20
         stock[1] = stock[1] + 40
         stock[2] = stock[2] + 15
         sales_menu(drinks, amount)
     elif value == '4':
         last_stock = check_stock()
-        if last_stock > stock:
-            print()
-            print(f'Order sent & {amount} paid')
-            update_sales(drinks, amount)
-            update_stock(stock)
-            print()
-            main()
-        else:
-            print()
-            print('Not enough stock, please RESTOCK')
-            print()
-            #Restock()
+        if last_stock[0] > stock[0]:
+            if last_stock[1] > stock[1]:
+                if last_stock[2] > stock[2]:
+                    print()
+                    print(f'Order sent & {amount} paid')
+                    update_sales(drinks, amount)
+                    update_stock(stock)
+                    print()
+                    main()
+                else:
+                    not_enough_stock()
+                    restock()
     else:
         wrong_selection()
         sales_menu(drinks, amount)
@@ -105,6 +114,66 @@ def sales_menu(drinks, amount):
     selector = input('Typed: ')
     item_added (selector, amount)
 
+def restock_item(what,how_much):
+    if what.lower() == 'coffeebeans':
+        last_stock = check_stock()
+        last_stock[0] = last_stock[0] + int(how_much)
+        stock_worksheet = SHEET.worksheet('stock')
+        stock_worksheet.append_row(last_stock)
+    elif what.lower() == 'milk':
+        last_stock = check_stock()
+        last_stock[1] = last_stock[1] + int(how_much)
+        stock_worksheet = SHEET.worksheet('stock')
+        stock_worksheet.append_row(last_stock)
+    elif what.lower() == 'sugar':
+        last_stock = check_stock()
+        last_stock[2] = last_stock[2] + int(how_much)
+        stock_worksheet = SHEET.worksheet('stock')
+        stock_worksheet.append_row(last_stock)
+    else:
+        print('Unknow ERROR')
+    restock()
+    
+def restock():
+    print()
+    print('Type (1) to restock')
+    print('Type (2) return to Main Menu')
+    selector = input('Typed: ')
+    if selector == '1':
+        print('What do you want to restock (list from: CoffeeBeans / Milk / Sugar) and how much?')
+        print('Exaple: Milk, 1000')
+        what, how_much = input('Type here : ').split(',')
+        print()
+        restock_item(what,how_much)
+    elif selector == '2':
+        main()
+    else:
+        wrong_selection()
+        restock()
+         
+def stock_menu():
+    print()
+    print('Stock Menu')
+    print()
+    print('Type (1) to Check Stock')
+    print('Type (2) to Restock')
+    print('Type (3) to back to Main Menu')
+    selector = input('Typed: ')
+    if selector == '1':
+        print()
+        print('Please wait...')
+        last_stock = check_stock()
+        print(f'Coffee beans remains {last_stock[0]} packs')
+        print(f'Milk remains {last_stock[1]} ml')
+        print(f'Sugar remains {last_stock[0]} packs')
+        stock_menu()
+    elif selector == '2':
+        restock()
+    elif selector == '3':
+        main()
+    else:
+        wrong_selection()
+
 while True:
     main()
     amount = 0
@@ -115,7 +184,7 @@ while True:
     if selector == '1':        
         sales_menu(drinks, amount)            
     elif selector == '2':
-        print('Stock Menu')
+        stock_menu()
     elif selector == '3':
         print('End of the day count')
     elif selector == '4':
